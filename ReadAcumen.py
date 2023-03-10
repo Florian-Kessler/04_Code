c81ef4c1f8b9f1c69d8312ae299466203298f31d
Florian Kessler <florian.kessler@unibe.ch>
2023-03-10 16:10:05 +0100

    update

--- ReadAcumen.py
+++ ReadAcumen.py
@@ -8,42 +8,42 @@ import os
 
 def read_RFnodeFile(file_):
     # read data from text file
-    df = np.loadtxt(file_, delimiter=',')
+    df_ = np.loadtxt(file_, delimiter=',')
 
     # Contains the frame number:
-    # frame = np.array(df[:, 0])
+    # frame = np.array(df_[:, 0])
 
     # reaction moment of the implant:
-    # rmx_ = np.array(df[:, 1])
-    # rmy_ = np.array(df[:, 2])
-    # rmz_ = np.array(df[:, 3])
+    # rmx_ = np.array(df_[:, 1])
+    # rmy_ = np.array(df_[:, 2])
+    # rmz_ = np.array(df_[:, 3])
 
     # reaction force of the implant:
-    # rfx_ = np.array(df[:, 4])
-    rfy_ = np.array(df[:, 5])
-    # rfz_ = np.array(df[:, 6])
+    # rfx_ = np.array(df_[:, 4])
+    rfy_ = np.array(df_[:, 5])
+    # rfz_ = np.array(df_[:, 6])
 
     # transverse disp. of the implant:
-    # ux_ = np.array(df[:, 7])
-    uy_ = np.array(df[:, 8])
-    # uz_ = np.array(df[:, 9])
+    # ux_ = np.array(df_[:, 7])
+    uy_ = np.array(df_[:, 8])
+    # uz_ = np.array(df_[:, 9])
 
     # rotational disp. of the implant:
-    # urx_ = np.array(df[:, 10])
-    # ury_ = np.array(df[:, 11])
-    # urz_ = np.array(df[:, 12])
+    # urx_ = np.array(df_[:, 10])
+    # ury_ = np.array(df_[:, 11])
+    # urz_ = np.array(df_[:, 12])
 
     return uy_, rfy_
 
 
 def read_acumen(file_a):
-    df = pd.read_csv(file_a, delimiter=';', skiprows=[0, 2])
-    # t_ = pd.DataFrame(df, columns=['Time ']).to_numpy()
-    d_ = pd.DataFrame(df, columns=['Axial Displacement ']).to_numpy()
+    df_ = pd.read_csv(file_a, delimiter=';', skiprows=[0, 2])
+    # t_ = pd.DataFrame(df_, columns=['Time ']).to_numpy()
+    d_ = pd.DataFrame(df_, columns=['Axial Displacement ']).to_numpy()
     # d_ = d_ - d_[0]  # calibrate displacement to zero at beginning
-    f_ = pd.DataFrame(df, columns=['Axial Force ']).to_numpy()
-    # f_set_ = pd.DataFrame(df, columns=['Axial Force Command ']).to_numpy()
-    cycle_ = pd.DataFrame(df, columns=['Axial Count ']).to_numpy()
+    f_ = pd.DataFrame(df_, columns=['Axial Force ']).to_numpy()
+    # f_set_ = pd.DataFrame(df_, columns=['Axial Force Command ']).to_numpy()
+    cycle_ = pd.DataFrame(df_, columns=['Axial Count ']).to_numpy()
     # arr_ = 0
     peak_ = np.zeros(int(np.max(cycle_)))
     vall_ = np.zeros(int(np.max(cycle_)))
@@ -78,15 +78,27 @@ plt.close('all')
 fig, ax1 = plt.subplots(1, 1, figsize=(9, 6))
 ax2 = ax1.twinx()
 
-PEEK1 = 'Pilot1/S131840_L4_S1_PEEK.csv'
-TITAN1 = 'Pilot1/S191840_L4_S2_DPS.csv'
-PEEK3 = 'Pilot3/ICOTEC_S130672_L5_icotec_accumen.csv'
-TITAN3 = 'Pilot3/ICOTEC_S130672_L5_DPS_accumen.csv'
+specimen = '05_Pilot5'
+
+# PEEK1 = '01_Pilot1/S131840_L4_S1_PEEK.csv'
+# TITAN1 = '01_Pilot1/S191840_L4_S2_DPS.csv'
+PEEK = ''
+TITAN = ''
+cor = 0
+if '03' in specimen:
+    PEEK = '03_Pilot3/ICOTEC_S130672_L5_icotec_accumen.csv'
+    TITAN = '03_Pilot3/ICOTEC_S130672_L5_DPS_accumen.csv'
+    cor = float(2.0)  # Correction for displacement offset for Ti
+elif '04' in specimen:
+    PEEK = '04_Pilot4/ICOTEC_S130672_L4_icotec_accumen.csv'
+    TITAN = '04_Pilot4/ICOTEC_S130672_L4_icotec_kwire_accumen.csv'
+elif '05' in specimen:
+    PEEK = '05_Pilot5/ICOTEC_S130684_L4_accumen.csv'
+    TITAN = '05_Pilot5/ICOTEC_S130684_L4_kwire_accumen.csv'
+
 loc = '/home/biomech/Documents/01_Icotec/01_Experiments/00_Data/'
 
-cor = float(2.0)  # Correction for displacement offset
-
-file = [loc + PEEK3, loc + TITAN3]
+file = [loc + PEEK, loc + TITAN]
 exp = {}
 col = [['#1f6a06', '#0a1eb2'],
        ['#373737', '#b20a0a']]
@@ -113,26 +125,40 @@ ax2.spines.right.set_visible(True)
 ax1.set_xlabel('Cycle Number')
 ax1.set_ylabel('Displacement / mm')
 ax2.set_ylabel('Force / N')
-ax1.axis([0, int(np.ceil(np.max(exp['cycle' + labels[1]])/1000)*1000),  -25, 0])
-ax2.axis([0, int(np.ceil(np.max(exp['cycle' + labels[1]])/1000)*1000), -750, 0])
+ax1.axis([0, int(np.ceil(np.max(exp['cycle' + labels[1]])/1000)*1000),  -10, 0])
+ax2.axis([0, int(np.ceil(np.max(exp['cycle' + labels[1]])/1000)*1000), -300, 0])
 ax2.legend(['PEEK Force', 'PEEK Displacement', 'Ti Force', 'Ti Displacement'])
 
-cyc1T = find_first(exp['fTi'][exp['peakTi']], -75)
-cyc2T = find_first(exp['fTi'][exp['peakTi']], -150)
-cyc1P = find_first(exp['fPEEK'][exp['peakPEEK'][:1330]], -75)
-cyc2P = find_first(exp['fPEEK'][exp['peakPEEK'][:1330]], -150)
+try:
+    cyc1T = find_first(exp['fTi'][exp['peakTi']], -75)
+    try:
+        cyc2T = find_first(exp['fTi'][exp['peakTi']], -150)
+    except StopIteration:
+        cyc2T = 0
+except StopIteration:
+    cyc1T = 0
+    cyc2T = 0
 
+try:
+    cyc1P = find_first(exp['fPEEK'][exp['peakPEEK']], -75)
+    try:
+        cyc2P = find_first(exp['fPEEK'][exp['peakPEEK']], -150)
+    except StopIteration:
+        cyc2P = 0
+except StopIteration:
+    cyc1P = 0
+    cyc2P = 0
 
 fig1, figP = plt.subplots(1, 1, figsize=(9, 6))
 plt.title('PEEK screw')
-fig2, figT = plt.subplots(1, 1, figsize=(9, 6))
-plt.title('Ti screw')
+# fig2, figT = plt.subplots(1, 1, figsize=(9, 6))
+# plt.title('Ti screw')
 col = ['#0072BD', '#D95319', '#EDB120', '#7E2F8E', '#77AC30', '#4DBEEE', '#A2142F', '#A214CC', '#A2DD2F']
 
 # number = ['00', '01', '02', '10', '11', '12']
-number = ['50', '55']
+number = ['50']  # , '55']
 for i in range(len(number)):
-    loc = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/03_Pilot3/'
+    loc = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/' + specimen + '/'
     folder = [filename for filename in os.listdir(loc) if filename.startswith(number[i])][0] + '/'
     samples = [filename for filename in os.listdir(loc + folder + '/') if filename.endswith('RFnode.txt')
                and '_02_' in filename]
@@ -144,8 +170,8 @@ for i in range(len(number)):
     Sim_mat = ['P', 'T']
     Fpoint = (-np.array([0, 75, 150])) * -37.037 - 1851
     for j in range(len(samples)):
-        lab = 'Screw excess = ' + folder.split('_S')[-1][0] + '.' + folder.split('_S')[-1][1] + ' mm, ' + \
-              'diameter = ' + folder.split('_D')[-1][0] + '.' + folder.split('_D')[-1][1] + ' mm'
+        # lab = 'Screw excess = ' + folder.split('_S')[-1][0] + '.' + folder.split('_S')[-1][1] + ' mm, ' + \
+        #       'diameter = ' + folder.split('_D')[-1][0] + '.' + folder.split('_D')[-1][1] + ' mm'
         lab = 'Unidirectional'
         if '55' in number[i]:
             lab = 'Toggling'
@@ -157,92 +183,52 @@ for i in range(len(number)):
             if rfy[-1] > 15:
                 figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
         elif 'T_T' in samples[j]:
-            figT.plot(-uy, rfy, color=col[i], linestyle='solid', label=lab)
+            figP.plot(-uy, rfy, color=col[i], linestyle='solid', label=lab)  # figT
             if rfy[-1] > 15:
-                figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
-        #elif 'P_T' in samples[j]:
-        #    figT.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_')
-        #    if rfy[-1] > 15:
-        #        figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
-        #elif 'T_P' in samples[j]:
-        #    figP.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_')
-        #    if rfy[-1] > 15:
-        #        figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
+                figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')  # figT
+        elif 'P_T' in samples[j]:
+            figP.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_')  # figT
+            if rfy[-1] > 15:
+                figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')  # figT
+        elif 'T_P' in samples[j]:
+            figP.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_')
+            if rfy[-1] > 15:
+                figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
         else:
             print('\n . . . Invalid file!\n')
 
-'''number = ['00', '01', '02', '10']  # , '11', '12']
-for i in range(len(number)):
-    loc = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/03_Pilot3/'
-    folder = [filename for filename in os.listdir(loc) if filename.startswith(number[i])][0] + '/'
-    samples = [filename for filename in os.listdir(loc + folder + '/') if filename.endswith('RFnode.txt')
-               and '_07_' in filename]
-    [uy, rfy] = 2*[0]
-    screw_force = np.zeros([5, 21])
-    ang = np.zeros([5, 21])
-    Screw_mat = ['P', 'T']
-    Sim_mat = ['P', 'T']
-    Fpoint = (-np.array([0, 75, 150])) * -37.037 - 1851
-    for j in range(len(samples)):
-        lab = 'Screw excess = ' + folder.split('_S')[-1][0] + '.' + folder.split('_S')[-1][1] + ' mm, ' + \
-              'diameter = ' + folder.split('_D')[-1][0] + '.' + folder.split('_D')[-1][1] + ' mm'
-        file_path = loc + folder + samples[j]
-        [uy, r_] = read_RFnodeFile(file_path)
-        [u_, rfy] = read_RFnodeFile(file_path.split('.txt')[0] + 'Fix.txt')
-        if 'P_P' in samples[j]:
-            figP.plot(-uy, rfy, color=col[i], linestyle='dotted', label='_nolegend_', alpha=0.5)
-            if rfy[-1] > 30:
-                figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_', alpha=0.5)
-        elif 'T_T' in samples[j]:
-            figT.plot(-uy, rfy, color=col[i], linestyle='dotted', label='_nolegend_', alpha=0.5)
-            if rfy[-1] > 30:
-                figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_', alpha=0.5)
-        elif 'P_T' in samples[j]:
-            figT.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_', alpha=0.5)
-            if rfy[-1] > 30:
-                figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_', alpha=0.5)
-        elif 'T_P' in samples[j]:
-            figP.plot(-uy, rfy, color=col[i], linestyle='dashdot', label='_nolegend_', alpha=0.5)
-            if rfy[-1] > 30:
-                figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_', alpha=0.5)
-        else:
-            print('\n . . . Invalid file!\n')'''
+# figP.plot(-1E5, -1E5, color='k', label='Tested side')
+# figP.plot(-1E5, -1E5, color='k', linestyle='dashdot', label='Collateral side')
+# figT.plot(-1E5, -1E5, color='k', label='Tested side')
+# figT.plot(-1E5, -1E5, color='k', linestyle='dashdot', label='Collateral side')
 
-#figP.plot(-1E5, -1E5, color='k', label='Tested side')
-#figP.plot(-1E5, -1E5, color='k', linestyle='dashdot', label='Collateral side')
-#figT.plot(-1E5, -1E5, color='k', label='Tested side')
-#figT.plot(-1E5, -1E5, color='k', linestyle='dashdot', label='Collateral side')
+if cyc2P:
+    figP.scatter(-exp['dPEEK'][exp['peakPEEK'][:cyc2P+1000]],
+                 -exp['fPEEK'][exp['peakPEEK'][:cyc2P+1000]], color='k', s=0.5, label='Experiment')
+else:
+    figP.scatter(-exp['dPEEK'][exp['peakPEEK']],
+                 -exp['fPEEK'][exp['peakPEEK']], color='k', s=0.5, label='Experiment')
+if cyc2T:
+    figP.scatter(-exp['dTi'][exp['peakTi'][:cyc2T+1000]],  # figT, 'k'
+                 -exp['fTi'][exp['peakTi'][:cyc2T+1000]], color='r', s=0.5, label='Experiment, corr.: '+str(cor)+' mm')
+else:
+    figP.scatter(-exp['dTi'][exp['peakTi']],  # figT, 'k'
+                 -exp['fTi'][exp['peakTi']], color='r', s=0.5, label='Experiment, corr.: ' + str(cor) + ' mm')
 
-'''
-figP.plot(-exp['dPEEK'][exp['peakPEEK'][cyc1P]-39:exp['peakPEEK'][cyc1P]+39],
-          -exp['fPEEK'][exp['peakPEEK'][cyc1P]-39:exp['peakPEEK'][cyc1P]+39], color='k', label='_nolegend_')
-figP.plot(-exp['dPEEK'][exp['peakPEEK'][cyc2P]-39:exp['peakPEEK'][cyc2P]+39],
-          -exp['fPEEK'][exp['peakPEEK'][cyc2P]-39:exp['peakPEEK'][cyc2P]+39], color='k', label='_nolegend_')
-figT.plot(-exp['dTi'][exp['peakTi'][cyc1T]-39:exp['peakTi'][cyc1T]+39],
-          -exp['fTi'][exp['peakTi'][cyc1T]-39:exp['peakTi'][cyc1T]+39], color='k', label='_nolegend_')
-figT.plot(-exp['dTi'][exp['peakTi'][cyc2T]-39:exp['peakTi'][cyc2T]+39],
-          -exp['fTi'][exp['peakTi'][cyc2T]-39:exp['peakTi'][cyc2T]+39], color='k', label='_nolegend_')
-'''
-
-figP.scatter(-exp['dPEEK'][exp['peakPEEK'][:cyc2P+1000]],
-             -exp['fPEEK'][exp['peakPEEK'][:cyc2P+1000]], color='k', s=0.5, label='Experiment')
-figT.scatter(-exp['dTi'][exp['peakTi'][:cyc2T+1000]],
-             -exp['fTi'][exp['peakTi'][:cyc2T+1000]], color='k', s=0.5, label='Experiment, corr.: ' + str(cor) + ' mm')
-
-figP.axis([-30, 30, -180, 180])
-figT.axis([-15, 15, -180, 180])
+figP.axis([0, 15, 0, 180])  # 30
+figP.axis([0, 15, 0, 180])  # figP
 figP.set_xlabel('Displacement / mm')
 figP.set_ylabel('Force / N')
-figT.set_xlabel('Displacement / mm')
-figT.set_ylabel('Force / N')
+figP.set_xlabel('Displacement / mm')  # figP
+figP.set_ylabel('Force / N')  # figP
 figP.legend(loc='lower right')
-figT.legend(loc='lower right')
+figP.legend(loc='lower right')  # figP
 
-
-#temp
+'''
+# temp #
 fig5, ax5 = plt.subplots(1, 1, figsize=(9, 6))
-file_ = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/03_Pilot3/50_L50_S00_D30/E_PEEK.txt'
-df = np.loadtxt(file_, delimiter='\t')
+file = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/03_Pilot3/50_L50_S00_D30/E_PEEK.txt'
+df = np.loadtxt(file, delimiter='\t')
 timeX = np.array(df[:, 0])
 strain = np.array(df[:, 1])
 plt.plot(timeX[40:]-1, strain[40:])
@@ -251,3 +237,4 @@ plt.xlabel('Step time')
 plt.ylabel('Logarithmic strain')
 
 print('\nRuntime: ' + str(round(time.time() - t1, 2)) + ' seconds.')
+'''
