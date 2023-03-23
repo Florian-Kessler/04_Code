29e5d4d39a3db4eb70bfdaf9a6741057ceec4604
Florian Kessler <florian.kessler@unibe.ch>
2023-03-23 07:15:04 +0100

    evaluate aramis, finding identical time points. working

--- test_delete.py
+++ test_delete.py
@@ -39,8 +39,10 @@ def read_RFnodeFile(file_):
 
 
 def read_ARAMIS(file_A):
-    df_ = pd.read_csv(file_A, delimiter=';', skiprows=[0])
-    stop = np.where(df_.isnull())[0][0]
+    stop = 0
+    df_ = pd.read_csv(file_A, delimiter=';', skiprows=[0])  # , quoting=csv.QUOTE_NONE, error_bad_lines=False)
+    if np.isnan(np.array(df_)[-1][-1]):
+        stop = np.where(df_.isnull())[0][0]
     lx = pd.DataFrame(df_, columns=['RodCsys→BoneCsys.LX [mm]'])
     ly = pd.DataFrame(df_, columns=['RodCsys→BoneCsys.LY [mm]'])
     lz = pd.DataFrame(df_, columns=['RodCsys→BoneCsys.LZ [mm]'])
@@ -48,8 +50,11 @@ def read_ARAMIS(file_A):
     thetaY = pd.DataFrame(df_, columns=['RodCsys→BoneCsys.Theta(Y) [°]'])
     psiZ = pd.DataFrame(df_, columns=['RodCsys→BoneCsys.Psi(Z) [°]'])
     t_ = pd.DataFrame(df_, columns=['Time UTC'])
-
-    return lx[:stop], ly[:stop], lz[:stop], phiX[:stop], thetaY[:stop], psiZ[:stop], t_[:stop]
+    print(len(lx))
+    if stop:
+        return lx[:stop], ly[:stop], lz[:stop], phiX[:stop], thetaY[:stop], psiZ[:stop], t_[:stop]
+    else:
+        return lx, ly, lz, phiX, thetaY, psiZ, t_
 
 
 def read_acumen(file_a):
@@ -94,28 +99,28 @@ plt.close('all')
 # # # # # INPUT # # # # #
 loc = '/home/biomech/Documents/01_Icotec/01_Experiments/00_Data/'
 specimen = '05_Pilot5'
-number = ['03', '13']  # simulations
+number = ['13']  # , '13']  # simulations
 
 fig1, figP = plt.subplots(1, 1, figsize=(9, 6))
-plt.title('PEEK (YM = 15 GPa')
+plt.title('PEEK (YM = 15 GPa)')
 fig2, figT = plt.subplots(1, 1, figsize=(9, 6))
-plt.title('Ti (YM = 100 GPa')
+plt.title('Ti (YM = 100 GPa)')
 col = ['#0072BD', '#D95319', '#EDB120', '#7E2F8E', '#77AC30', '#4DBEEE', '#A2142F', '#A214CC', '#A2DD2F']
 
 no = specimen.split('_')[0]
 folder = [filename for filename in os.listdir(loc) if filename.startswith(no)][0] + '/'
 sampleIPD = ([filename for filename in os.listdir(loc + folder)
-             if 'aramis' in filename and 'icotec' in filename and 'kwire' not in filename] or [None])[0]
+              if 'aramis' in filename and 'icotec' in filename and 'kwire' not in filename] or [None])[0]
 sampleIPF = ([filename for filename in os.listdir(loc + folder)
-             if 'acumen' in filename and 'icotec' in filename and 'kwire' not in filename] or [None])[0]
+              if 'acumen' in filename and 'icotec' in filename and 'kwire' not in filename] or [None])[0]
 sampleIKD = ([filename for filename in os.listdir(loc + folder)
-             if 'aramis' in filename and 'kwire' in filename] or [None])[0]
+              if 'aramis' in filename and 'kwire' in filename] or [None])[0]
 sampleIKF = ([filename for filename in os.listdir(loc + folder)
-             if 'acumen' in filename and 'kwire' in filename] or [None])[0]
+              if 'acumen' in filename and 'kwire' in filename] or [None])[0]
 sampleTiD = ([filename for filename in os.listdir(loc + folder)
-             if 'aramis' in filename and 'DPS' in filename] or [None])[0]
+              if 'aramis' in filename and 'DPS' in filename] or [None])[0]
 sampleTiF = ([filename for filename in os.listdir(loc + folder)
-             if 'acumen' in filename and 'DPS' in filename] or [None])[0]
+              if 'acumen' in filename and 'DPS' in filename] or [None])[0]
 samplesD = sampleIPD, sampleIKD, sampleTiD
 samplesF = sampleIPF, sampleIKF, sampleTiF
 label_screw = ['Icotec', 'Icotec2', 'DPS']
@@ -156,20 +161,20 @@ for i in range(len(samplesD)):
         if len(peakAc) > len(peak):
             start = len(peakAc) - len(peak)
             if i == 0 or 1:
-                figP.scatter(-y[peak], -F[peakAc[start:]], color=col[i], s=1)
+                figP.scatter(-y[peak], -F[peakAc[start:]], color=col[i], s=1, label='Experiment ' + label_screw[i])
             elif i == 2:
-                figT.scatter(-y[peak], -F[peakAc[start:]], color=col[i], s=1)
+                figT.scatter(-y[peak], -F[peakAc[start:]], color=col[i], s=1, label='Experiment ' + label_screw[i])
         elif len(peak) > len(peakAc):
             start = len(peak) - len(peakAc)
             if i == 0 or 1:
-                figP.scatter(-y[peak[start:]], -F[peakAc], color=col[i], s=1)
+                figP.scatter(-y[peak[start:]], -F[peakAc], color=col[i], s=1, label='Experiment ' + label_screw[i])
             elif i == 2:
-                figT.scatter(-y[peak[start:]], -F[peakAc], color=col[i], s=1)
+                figT.scatter(-y[peak[start:]], -F[peakAc], color=col[i], s=1, label='Experiment ' + label_screw[i])
         else:
             if i == 0 or 1:
-                figP.scatter(-y[peak], -F[peakAc], color=col[i], s=1)
+                figP.scatter(-y[peak], -F[peakAc], color=col[i], s=1, label='Experiment ' + label_screw[i])
             elif i == 2:
-                figT.scatter(-y[peak], -F[peakAc], color=col[i], s=1)
+                figT.scatter(-y[peak], -F[peakAc], color=col[i], s=1, label='Experiment ' + label_screw[i])
 
 
 loc = '/home/biomech/Documents/01_Icotec/02_FEA/98_Pilots/' + specimen + '/'
@@ -187,39 +192,33 @@ for i in range(len(number)):
         lab = 'Screw excess = ' + folder.split('_S')[-1][0] + '.' + folder.split('_S')[-1][1] + ' mm, ' \
               # + 'diameter = ' + folder.split('_D')[-1][0] + '.' + folder.split('_D')[-1][1] + ' mm'
         file_path = loc + folder + samples[j]
-        [uy, rf_] = read_RFnodeFile(file_path)  # switch for inverse loading
-        [u_, rfy] = read_RFnodeFile(file_path.split('.txt')[0] + 'Fix.txt')  # switch for inverse loading
+        [uy, rf_] = read_RFnodeFile(file_path)
+        [u_, rfy] = read_RFnodeFile(file_path.split('.txt')[0] + 'Fix.txt')
         if 'P_P' in samples[j]:
-            figP.plot(-uy, rfy, color=col[i], linestyle='solid', label='_nolegend_')
+            figP.plot(-uy, rfy, color=col[i], linestyle='solid', label='FE Icotec')
             if rfy[-1] > 20:
                 figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
         elif 'T_T' in samples[j]:
-            figT.plot(-uy, rfy, color=col[i], linestyle='solid', label='_nolegend_')  # figT
+            figT.plot(-uy, rfy, color=col[i], linestyle='solid', label='DPS')  # figT
             if rfy[-1] > 20:
                 figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')  # figT
         elif 'P_T' in samples[j]:
-            figT.plot(-uy, rfy, color=col[i], linestyle='dashed', label='_nolegend_')  # figT
+            figT.plot(-uy, rfy, color=col[i], linestyle='dashed', label='DPS (C)')  # figT
             if rfy[-1] > 20:
                 figT.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')  # figT
         elif 'T_P' in samples[j]:
-            figP.plot(-uy, rfy, color=col[i], linestyle='dashed', label='_nolegend_')
+            figP.plot(-uy, rfy, color=col[1], linestyle='solid', label='FE Icotec2')  # here:
+            # changed dashed to solid and color i to 1
             if rfy[-1] > 20:
                 figP.scatter(-uy[-1], rfy[-1], color='k', marker='x', label='_nolegend_')
         else:
             print('\n . . . Invalid file!\n')
-figP.axis([0, 12, 0, 350])
-figT.axis([0, 12, 0, 350])
-# figP.plot(-1E5, -1E5, color='k', label='Tested side')
-# figP.plot(-1E5, -1E5, color='k', linestyle='dashed', label='Collateral side')
-# figP.plot(-1E5, -1E5, color=col[0], label='Screw-excess = 5 mm')
-# figP.plot(-1E5, -1E5, color=col[1], label='Screw-excess = 0 mm')
-# figT.plot(-1E5, -1E5, color='k', label='Tested side')
-# figT.plot(-1E5, -1E5, color='k', linestyle='dashed', label='Collateral side')
-# figT.plot(-1E5, -1E5, color=col[0], label='Screw-excess = 5 mm')
-# figT.plot(-1E5, -1E5, color=col[1], label='Screw-excess = 0 mm')
-plt.legend()
+figP.axis([0, 10.5, 0, 250])
+figT.axis([0, 10.5, 0, 350])
 
-#%%
+figP.legend()
+
+
 '''
 fileAramis = '/home/biomech/Documents/01_Icotec/01_Experiments/00_Data/05_Pilot5/ICOTEC_S130684_L4_aramis.csv'
 fileAcumen = '/home/biomech/Documents/01_Icotec/01_Experiments/00_Data/05_Pilot5/ICOTEC_S130684_L4_accumen.csv'
