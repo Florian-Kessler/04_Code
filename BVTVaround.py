92c38b73697cd23d7004bb19976fe393c21bca24
Florian Kessler <florian.kessler@unibe.ch>
2023-06-26 11:10:15 +0200

    update, bvtv around screw

--- BVTVaround.py
+++ BVTVaround.py
@@ -8,12 +8,14 @@ import os
 import ReadRawMHD as rR
 
 
-def eval_bvtv(sample):
+def eval_bvtv(sample, radius):
     t1 = time.time()
+    check = 0
     sample_code = sample
     path_project = '/home/biomech/Documents/01_Icotec/'  # General project folder
     path_ct = path_project + '01_Experiments/02_Scans/' + sample_code + '/04_Registered/'  # Folder of CT dat
-    file_bone = [filename for filename in os.listdir(path_ct + '/') if filename.endswith('image.mhd') and str(sample_code) in filename][0]
+    file_bone = [filename for filename in os.listdir(path_ct + '/') if filename.endswith('image.mhd')
+                 and str(sample_code) in filename][0]
     file = path_ct + file_bone
 
     bone_grey = sitk.ReadImage(file)
@@ -22,43 +24,42 @@ def eval_bvtv(sample):
     check_image = rR.zeros_and_ones(bone_img, 320)
     res = max(np.array(bone_grey.GetSpacing()))
     ori = abs((np.array(bone_grey.GetOrigin())/res).astype(int))
-    print(res)
 
     # Area to evaluate
-    r_mm = 6  # radius in mm
+    r_mm = radius  # radius in mm
     r = int(np.rint(r_mm / res))
     length = np.rint(np.array([-45, 0]) / res).astype(int)
     drill = int(1.4/res)  # radius drill
 
     b = 0
     o = 0
-    ii = 0
-    jj = 0
     for z in range(min(length), max(length)):
         for y in range(-r, r):
             for x in range(-r, r):
                 if r**2 >= x**2 + y**2 > drill**2:
-                    check_image[x+ori[0], y+ori[1], z+ori[2]] = check_image[x+ori[0], y+ori[1], z+ori[2]] + 3
+                    check_image[x+ori[0], y+ori[1], z+ori[2]] = check_image[x+ori[0], y+ori[1], z+ori[2]] + 2
                     if bone_bvtv[x+ori[0], y+ori[1], z+ori[2]] == 1:
                         b = b+1
                     elif bone_bvtv[x+ori[0], y+ori[1], z+ori[2]] == 0:
                         o = o+1
                     else:
                         print('**ERROR**')
+
     bvtv = round(b/(b+o), 3)
-    print('BV/BV: ' + str(bvtv))
-    plt.figure()
-    plt.imshow(check_image[:, :, 800])
-    plt.figure()
-    plt.imshow(check_image[:, :, 600])
-    plt.figure()
-    plt.imshow(check_image[:, :, 400])
-    plt.figure()
-    plt.imshow(check_image[:, :, 200])
-    plt.figure()
-    plt.imshow(check_image[:, ori[1], :])
-    plt.figure()
-    plt.imshow(check_image[ori[0], :, :])
+    if check:
+        print('BV/BV: ' + str(bvtv))
+        plt.figure()
+        plt.imshow(check_image[:, :, 800])
+        plt.figure()
+        plt.imshow(check_image[:, :, 600])
+        plt.figure()
+        plt.imshow(check_image[:, :, 400])
+        plt.figure()
+        plt.imshow(check_image[:, :, 200])
+        plt.figure()
+        plt.imshow(check_image[:, ori[1], :])
+        plt.figure()
+        plt.imshow(check_image[ori[0], :, :])
     tRun = time.time() - t1
     if tRun >= 3600:
         print('Execution time: ' + str(int(tRun / 3600)) + ' h ' + str(int(np.mod(tRun, 3600) / 60)) + ' min ' +
@@ -81,7 +82,26 @@ sample_list = ['S130684_L2_left', 'S130684_L2_right',
                'S131840_L1_left', 'S131840_L1_right', 'S131840_L2_left', 'S131840_L2_right',
                'S131840_L3_left', 'S131840_L3_right']
 
-i = 2
-BVTV = eval_bvtv(sample_list[i])
-print(sample_list[i])
-print(BVTV)
+try:
+    os.remove('/home/biomech/Documents/01_Icotec/01_Experiments/02_Scans/BVTV.txt')
+    print('Existing file has been deleted. Creating new file')
+except:
+    print('Creating new file')
+
+radius_mm = [3, 4, 5, 6]
+
+for k in range(2):
+    for j in range(len(radius_mm)):
+        for i in range(len(sample_list)):
+            BVTV = eval_bvtv(sample_list[i], radius_mm[j])
+            if k == 0:
+                with open('/home/biomech/Documents/01_Icotec/01_Experiments/02_Scans/BVTV_' + str(radius_mm[j]) +
+                          '.txt', 'a') as f:
+                    f.write(sample_list[i] + '\n')
+                    f.write(str(BVTV) + '\n')
+                f.close()
+            elif k == 1:
+                with open('/home/biomech/Documents/01_Icotec/01_Experiments/02_Scans/BVTV_' + str(radius_mm[j]) +
+                          's.txt', 'a') as f:
+                    f.write(str(BVTV) + '\n')
+                f.close()
