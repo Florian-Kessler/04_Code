56a979d21aac912384f540c52b45c541acd31898
Florian Kessler <florian.kessler@unibe.ch>
2023-07-27 14:11:24 +0200

    update

--- SCP_ubelix.py
+++ SCP_ubelix.py
@@ -11,10 +11,10 @@ def copy(spec, mod, doc, dir):
              + specimens[spec] + '/' + mod + '/'
     local = '/home/biomech/Documents/01_Icotec/02_FEA/01_MainStudy/' + specimens[spec] + '/' + mod + '/'
     pw = open('/home/biomech/Documents/99_General/P_fk.txt', 'r').read()
-    if dir == 0:
+    if dir == 'to':
         os.system('sshpass -p ' + pw + ' scp ' + local + doc + ' ' + user + '@' + server + remote)
         print(doc + ' successfully copied from ' + local + ' \nto \n' + user + '@' + server + remote + '.')
-    elif dir == 1:
+    elif dir == 'from':
         os.system('sshpass -p ' + pw + ' scp ' + user + '@' + server + remote + doc + ' ' + local)
         print(doc + ' successfully copied from ' + user + '@' + server + remote + ' \nto \n' + local + '.')
     else:
@@ -22,13 +22,13 @@ def copy(spec, mod, doc, dir):
 
 
 specimen = np.arange(0, 34)
-document = '*05*T*.txt'
-direction = 1  # 0 = from local to remote, 1 = from remote to local
-model = '81_L50_S50_D45'
+document = '*T.sh'
+direction = 'to'  # from local TO REMOTE or FROM REMOTE to local
+model = '87_L50_S50_D45'
 
 # peek_samples = [2, 5, 7, 8, 10, 13, 15, 16, 18, 21, 23, 24, 26, 29, 31, 32]
 # ti_samples = [3, 4, 6, 9, 11, 12, 14, 17, 19, 20, 22, 25, 27, 28, 30, 33]
 
 # [12, 14, 17, 19, 20, 22, 25, 27, 28, 30, 33]:  # range(12, 19):  # range(len(specimen)):
-for i in range(len(specimen)):
+for i in [3, 4, 6, 9, 11, 12, 14, 17, 19, 20, 22, 25, 27, 28, 30, 33]:
     copy(specimen[i], model, document, direction)
